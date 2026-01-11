b073f23efc2a94042d680c25d8a2be8c6265717b
aditi-sirpi <aditi@sirpi.io>
2026-01-11 20:52:38 +0530

    UI Issue fixed

--- frontend/app/(logged-in)/upload/page.tsx
+++ frontend/app/(logged-in)/upload/page.tsx
@@ -1,16 +1,28 @@
 import BgGradient from "@/components/common/bg-gradient";
 import UploadHeader from "@/components/upload/upload-header";
 import UploadForm from "@/components/upload/upload-form";
+import { syncUser } from "@/lib/users";
+import { redirect } from "next/navigation";
 
-export default function UploadPage() {
+export default async function UploadPage(props: {
+    searchParams: Promise<{ session_id?: string }>;
+}) {
+    const searchParams = await props.searchParams;
+    const { session_id } = searchParams;
+
+    if (session_id) {
+        await syncUser(session_id);
+        redirect('/upload');
+    }
+
     return (
         <BgGradient>
             <section className="min-h-screen">
                 <div className="mx-auto max-w-7xl px-6 pt-8 pb-16 lg:px-8">
-                   
-                   <div className="flex flex-col items-center justify-center gap-6 text-center">
-                   <UploadHeader />
-                    <UploadForm />
+
+                    <div className="flex flex-col items-center justify-center gap-6 text-center">
+                        <UploadHeader />
+                        <UploadForm />
                     </div>
                 </div>
             </section>
