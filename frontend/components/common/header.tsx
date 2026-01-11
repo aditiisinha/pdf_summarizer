de4f27d585625a9e442d432935f4381e8c9fe9e5
aditi-sirpi <aditi@sirpi.io>
2026-01-11 22:08:46 +0530

    summarium icon link added

--- frontend/components/common/header.tsx
+++ frontend/components/common/header.tsx
@@ -12,7 +12,7 @@ export default function Header({ navClassName = "" }: HeaderProps) {
     return (
         <nav className={`container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto gap-4 ${navClassName}`}>
             <div className="flex flex-1">
-                <Link href="/pricing" className="flex items-center gap-1 lg:gap-2 shrink-0">
+                <Link href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
                     <FileText className="w-8 h-8 text-gray-900 group-hover:rotate-12 transform transition duration-200 ease-in-out" />
                     <span className="font-extrabold text-xl text-gray-900">Summarium</span>
                 </Link>
